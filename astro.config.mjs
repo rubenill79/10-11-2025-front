459d093598586158ebb8f16322e8a8ea0c6721c8
Rubén Lázaro <rubenill79@hotmail.com>
2026-03-08 12:42:39 +0100

    Added SSR and i18n

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,21 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 import tailwindcss from '@tailwindcss/vite';
+import vercel from '@astrojs/vercel';
 
 // https://astro.build/config
 export default defineConfig({
+  output: "server",
+  site: 'https://10-11-2025-front.vercel.app',
+  adapter: vercel(),
   vite: {
     plugins: [tailwindcss()]
+  },
+  i18n: {
+    defaultLocale: "en",
+    locales: ["es", "en"],
+    routing: {
+      prefixDefaultLocale: true
+    }
   }
 });
---
+++ src/i18n/ui.ts
@@ -0,0 +1,17 @@
+export const languages = {
+    en: 'English',
+    es: 'Español',
+  };
+  
+  export const defaultLang = 'en';
+  
+  export const ui = {
+    en: {
+      'layout.title': '10-11-2025 - Home',
+      'header.title': '10-11-2025 💚',
+    },
+    es: {
+      'layout.title': '10-11-2025 - Inicio',
+      'header.title': '10-11-2025 💚',
+    },
+  } as const;
